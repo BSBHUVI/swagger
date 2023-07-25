86e61a8a7f8b9189fed9ff2503c3db197aa3814f
Bhuvan <sce19cs090@sairamtap.edu.in>
2023-07-25 12:34:17 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { swaggerUrls } from "./static/swaggerUrls.js";
 import Iframe from "./Iframe.js";
-import './App.css'
-import logo from './static/logo.png'
-import jciLogo from './static/jciLogo.png'
+import "./App.css";
+import logo from "./static/logo.png";
+import jciLogo from "./static/jciLogo.png";
 
 function App() {
   const [services, setServices] = useState([]);
@@ -12,47 +12,49 @@ function App() {
     setServices(Object.keys(swaggerUrls));
   }, []);
 
+  
+
   const loadSwagger = (val) => {
-    
     setSwaggerUrl(val);
   };
   return (
     <>
-  <div className="navbar">
-  <div className="jci">
-<img src={jciLogo} alt="logo" />
-  </div>
-  <div className="logo">
-<img src={logo} alt="logo" />
-  </div>
-  <div></div>
-  </div>
-    <div className="swagger-ui">
-      <div className="sidebar">
-     <div className="serviceHeader">
-     <h1 >Services</h1>
-     </div>
-        {services &&
-          services.map &&
-          services.map((value, key) => {
-            return (
-              <div key={key}>
-                <p
-                  className={` ${swaggerUrls[value]===swaggerUrl?"active":"NotActive"}`} 
-                  onClick={(e) =>{loadSwagger(swaggerUrls[value])}}
-                >
-                  {value}
-                </p>
-              </div>
-            );
-          })}
+      <div className="navbar">
+        <div className="jci">
+          <img src={jciLogo} alt="logo" />
+        </div>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div></div>
       </div>
+      <div className="swagger-ui">
+        <div className="sidebar">
+          <div className="serviceHeader">
+            <h1>Services</h1>
+          </div>
+          {services &&
+            services.map &&
+            services.map((value, key) => {
+              return (
+                <div key={key}>
+                  <p
+                    className={` ${
+                      swaggerUrls[value] === swaggerUrl ? "active" : "NotActive"
+                    }`}
+                    onClick={(e) => {
+                      loadSwagger(swaggerUrls[value]);
+                    }}
+                  >
+                    {value}
+                  </p>
+                </div>
+              );
+            })}
+        </div>
 
-     
-     <Iframe swaggerUrl={swaggerUrl}/>
-     
-    </div>
- 
+        <Iframe swaggerUrl={swaggerUrl} />
+      </div>
     </>
   );
 }
